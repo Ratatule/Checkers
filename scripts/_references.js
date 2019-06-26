828e465d9a06ae15941f4557687fb4ffe0921f70
TatulM <tatulm@ogmainc.com>
2019-06-26 14:52:00 +0400

    Commit

---
+++ scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-2.1.4.js" />
+/// <reference path="main.js" />
+/// <reference path="checkers.js" />
+/// <reference path="extensions.js" />
+/// <reference path="ui.manager.js" />
